50be716d8680d1665fed2d660c3b931be0a94561
ABZ187 <arbaazshaikh187@gmail.com>
2022-10-06 21:19:24 +0530

    heroku commit

--- QGate/QGate/settings.py
+++ QGate/QGate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#t&=rd4)*)n!zugoj(x7&u33%r&8&ih+yquqscu35aacy6mfar"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
