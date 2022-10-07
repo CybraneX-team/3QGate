33d082d0d328a6677fe25575c31130512cfce9f9
ABZ187 <arbaazshaikh187@gmail.com>
2022-10-07 21:23:47 +0530

    heroku commit 18 day2

--- 3Q-Gate/settings.py
+++ 3Q-Gate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','qocomposer.herokuapp.com'] #
+ALLOWED_HOSTS = ['localhost','qocomposer.herokuapp.com']
 
 # Application definition
 
